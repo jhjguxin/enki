d508c8be2744f9ab9dd1001d309addfa5839faf6
jhjguxin <864248765@qq.com>
2012-02-10 13:48:15 +0800

    use bootstrap for enki's front site page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def show
     @page = Page.find_by_slug(params[:id]) || raise(ActiveRecord::RecordNotFound)
-    add_breadcrumb("#{t("menu.pages")}: #{@page.title}",@show)
+    breadcrumbs.add("#{t("menu.pages")}: #{@page.title}",@show)
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,9 +6,12 @@ class SearchController < ApplicationController
 
       keywords search_text, :highlight => true
       with(:published_at).less_than Time.now
-      #fulltext search_text, :highlight => true
-      paginate :page => params[:page], :per_page => 10
+      fulltext search_text, :highlight => true
+      #paginate :page => params[:page], :per_page => 10
       order_by :published_at, :desc
+    end
+    unless @search.results.blank?
+      return @results_paginate=@search.results.paginate(params[:page])
     end
     #breakpoint
     set_seo_meta("#{t("menu.search")}: #{params[:q]}")
