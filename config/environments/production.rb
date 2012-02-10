2a2521ed7600b3cd82cfd0e76ed6efc3b9db977b
jhjguxin <864248765@qq.com>
2012-02-10 15:34:55 +0800

    Rails 3.1 and asset pipeline problems with Apache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,19 @@ Enki::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
+  # Compress both stylesheets and JavaScripts
+  config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :scss
+
+  #"Lighttpd has this feature and there is a mod_xsendfile for Apache2. Nginx also has this feature, but implemented a little bit differently. In Nginx this feature is called X-Accel-Redirect."
+  config.action_dispatch.x_sendfile_header = "X-Sendfile" 
+  config.serve_static_assets = true
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
 
+  #config.serve_static_assets = true
+  # Use 'X-Accel-Redirect' for nginx
+  #config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
